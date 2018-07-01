8f33d7db70db3ce665ea0e2a5c5a6b6698f423a5
capndave <capndavet@gmail.com>
2018-07-01 12:40:49 -0500

    Added input fields pt 1

---
+++ src/components/LandingPage/Forms/WidePgForm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function WidePgForm (props) {
+  console.log('WidePgForm working')
+  return (
+    <form>
+      <div>
+        WidePgForm
+        <input type="text" placeholder=""></input>
+        <input type="text"></input>
+      </div>
+    </form>
+  )
+}
+
+export default WidePgForm;
+
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
+import WidePgForm from './Forms/WidePgForm'
 import './LandingPage.css'
 
 var LandingPage = (props) => {
     return ( 
       <div className="LandingPage">
         <NavBar />
-        <h1 className="app-title">Lab-Rat</h1>
-        <form className="LandingPage-flex-form" autoComplete="off" >
-          <div className="LandingPage-group">
-          </div>
-        </form>
+        <h1 className="LandingPage-app-title">Lab-Rat</h1>
+        <WidePgForm/>
       </div>
     )
 } 
